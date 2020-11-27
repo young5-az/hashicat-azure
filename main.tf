1614d4041a3314cb1082a95b143a5c72c41047c9
young5-az <young5@cloocus.com>
2020-11-27 07:48:32 +0000

    Added vnet module

--- main.tf
+++ main.tf
@@ -94,12 +94,7 @@ resource "azurerm_virtual_machine" "catapp" {
   location            = var.location
   resource_group_name = azurerm_resource_group.myresourcegroup.name
   vm_size             = var.vm_size
-  #department = "devops"
-  #Billable = "true"
-    tags = {
-    department = "devops"
-    Billable  = "true"
-  }
+ 
 
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
@@ -128,7 +123,10 @@ resource "azurerm_virtual_machine" "catapp" {
     disable_password_authentication = false
   }
 
-  tags = {}
+  tags = {
+    department = "devops"
+    Billable  = "true"
+  }
 }
 
 # We're using a little trick here so we can run the provisioner without
